991f0efdd5c6994b7f72c8fb3d587fd3f48a0fc5
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-09-21 16:07:42 +0400

    feat: add change single if clickk is on the button next

--- audio-player/script.js
+++ audio-player/script.js
@@ -1,12 +1,19 @@
 const audioPlayBtn = document.querySelector('.play');
 const audioPauseBtn = document.querySelector('.pause');
+const playNextAudio = document.querySelector('.play_next');
+const textAuthor = document.querySelector('.single_author');
+const textNameSingle = document.querySelector('.single_name');
 
 let audio;
-
 let numberSong = 0;
 
 audioPlayBtn.addEventListener('click', playAudio);
 audioPauseBtn.addEventListener('click', pauseAudio);
+playNextAudio.addEventListener('click', () => {
+    playNext();
+    playAudio();
+    console.log(1);
+})
 
 const soundsAuthor = [
     "Five Finger Death Punch",
@@ -24,16 +31,23 @@ const soundsName = [
     "Someone to talk to"
 ];
 
+const soundLinks = [
+    `./assets/music/Five_Finger_Death_Punch.mp3`,
+    `./assets/music/OOMPH!.mp3`,
+    `./assets/music/Skillet.mp3`,
+    `./assets/music/Thousand_Foot_Krutch.mp3`,
+    `./assets/music/Three_Days_Grace.mp3`
+]
+
 function playAudio() {
+
     if (!audio) {
         audio = new Audio();
-        audio.src = `./assets/music/Five_Finger_Death_Punch.mp3`;
-        // audio.currentTime = 0;
+        audio.src = soundLinks[numberSong];
         audio.play();
-        console.log(audio.currentTime);
     }
 
-    // audio.src = `./assets/music/Five_Finger_Death_Punch.mp3`;
+    audio.src = soundLinks[numberSong];
     audio.play();
     audioPlayBtn.classList.remove('visible');
     audioPlayBtn.classList.add('hidden');
@@ -50,3 +64,19 @@ function pauseAudio() {
         audioPauseBtn.classList.remove('visible');
     }
 }
+
+function playNext() {
+    if(!audio) {
+        numberSong;
+    } else {
+        numberSong += 1;
+    }
+
+    if(numberSong > soundLinks.length - 1) {
+        numberSong = 0;
+    }
+
+    textAuthor.textContent = soundsAuthor[numberSong];
+    textNameSingle.textContent = soundsName[numberSong];
+}
+
